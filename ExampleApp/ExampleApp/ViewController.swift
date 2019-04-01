97323721e3f959ad2998696455dab971ff424a73
Amey Tavkar <amey.tavkar@gmail.com>
2019-04-01 12:51:27 +0530

    Add Example App for demontration

---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Amey Tavkar on 01/04/19.
+//  Copyright © 2019 Amey Tavkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
